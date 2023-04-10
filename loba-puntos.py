2d07b7adfd28b642627f45c525bfc231a13be75b
Mariano <mariano.ramaidana@gmail.com>
2023-04-09 21:58:45 -0300

    primer commit

---
+++ loba-puntos.py
@@ -0,0 +1,15 @@
+'''Programa que lleva la cuenta de los puntos de la Loba.
+Se puede definir el número de jugadores y 3 modalidades de juego para que finalice la partida de 
+manera automática.'''
+
+nombres = []
+i = 0
+cont = int(input("ingresar cantidad de jugadores: "))
+
+while i != cont:
+    for i in range(0,cont):
+        valor = input("ingrese un nombre: ")
+        nombres.append(valor)
+        i = i + 1
+
+print(nombres)
---
+++ pruebas.py
@@ -0,0 +1,12 @@
+#Añadir valores a una lista
+'''numeros = []
+i = 0
+cont = int(input("ingresar cantidad de valores: "))
+
+while i != cont:
+    for i in range(0,cont):
+        valor = int(input("ingrese un número: "))
+        numeros.append(valor)
+        i = i + 1
+
+print(numeros)'''
