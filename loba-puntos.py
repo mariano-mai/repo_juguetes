0f517b2c2e599eabbcb64f6cae24faeced6206fe
Mariano <mariano.ramaidana@gmail.com>
2023-04-14 21:56:42 -0300

    varios intentos de ingreso de valores

--- loba-puntos.py
+++ loba-puntos.py
@@ -28,17 +28,63 @@ for elemento in jugadores:
 
 #lista = []
 
+'''i = 0
+lista_aux = []
+while i < 3:
+    for elem in jugadores:
+        lista = []
+        #suma = 0
+        valor = int(input("ingrese un valor: "))
+        lista.append(valor)
+        lista_aux.extend(lista)
+        jugadores[elem].update({'puntos':lista_aux})
+    i = i + 1'''
+
+#añade la cantidad determinada de valores al jugador correspondiente, siguiendo el orden del diccionario (funciona)
+'''jug_nombre = int(input("ingrese condición (2): "))
 for elem in jugadores:
+    h = 0
+    lista_aux = []
+    while h < 3:
+        #lista_aux = []
+        if jug_nombre == 2:
+            lista = []
+            valor = int(input("ingrese un valor: "))
+            lista.append(valor)
+            lista_aux.extend(lista)
+            jugadores[elem].update({'puntos':lista_aux})
+        else:
+            print("pasó de largo")
+        h = h + 1'''
+
+#asigna 1 (UN, singular) valor a cada jugador sin importar el orden (pregunta primero) FUNCIONA!!!
+for elem in jugadores:
+    lista_aux = []
+    clave = input("ingrese nombre de un jugador: ").capitalize()
     lista = []
-    #suma = 0
     valor = int(input("ingrese un valor: "))
     lista.append(valor)
-    jugadores[elem].update({'puntos':lista})
+    lista_aux.extend(lista)
+    jugadores[clave].update({'puntos':lista_aux})
+
+#no funciona, sobreescribe los valores de cada jugador
+'''for elem in jugadores:
+    h = 0
+    #lista_aux = []
+    while h < 2:
+        clave = input("ingrese el nombre de un jugador: ").capitalize()
+        lista = []
+        lista_aux = []
+        valor2 = int(input("ingrese un valor: "))
+        lista.append(valor2)
+        lista_aux.extend(lista)
+        jugadores[clave].update({'puntos':lista_aux})
+        h = h + 1'''
 
 #esta parte es para que imprima los nombres en columnas
 cadena = ""
 for elem in jugadores:
     cadena = cadena + "algo"
-print(cadena)
+#print(cadena)
 
 print(jugadores)
